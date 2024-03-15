d1fbc97667419d934a5eab2125b88a35e0bf7184
Sarah Lagares <smlagares99@gmail.com>
2024-03-15 15:13:19 -0400

    Uploading content from lesson 2

--- src/com/coderscampus/lesson1/OptionalApplication.java
+++ src/com/coderscampus/lesson1/OptionalApplication.java
@@ -5,13 +5,18 @@ import java.util.Optional;
 public class OptionalApplication {
 
 	public static void main(String[] args) {
+		//lesson 1
 		Optional<String> fullName = Optional.of("Sarah Lagares");
-		
 		System.out.println(fullName);
-		
 		String sarah = fullName.get();
-		
 		System.out.println(sarah);
+		
+		//lesson 2+
+		Recipe cookies = new Recipe();
+		cookies.setName("Chocolate Chip Cookies");
+		cookies.setInstructions("1. Preheat oven\n2. Put dough on baking sheets\n3. Cook dough for 15 minutes");
+	
+		System.out.println(cookies);
 	}
 
 }
